698db133f70daa5abcbc23cfea985266746f1c55
hyun2eee <hyune_v@naver.com>
2024-11-27 18:05:22 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+	int i, j, N;
+    scanf("%d", &N);
+
+	for (i = 1; i < N+1; i++) {
+		for (j = 1; j < i + 1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
