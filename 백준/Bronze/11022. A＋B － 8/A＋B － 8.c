313b4a9d8d6a31d331ed82331d1ceaecfab0ff2d
hyun2eee <hyune_v@naver.com>
2024-11-27 18:22:57 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+
+	int i, num, a, b;
+	scanf("%d", &num);
+        for (i = 0; i < num; i++) {
+			scanf("%d%d", &a, &b);
+			printf("Case #%d: %d + %d = %d\n",i+1, a, b, a+b);
+	}
+	return 0;
+}
